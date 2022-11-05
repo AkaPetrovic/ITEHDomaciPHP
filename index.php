029fe533b8a6ccdcf67696d417409dd7f8ed980e
AkaPetrovic <akapetrovic@gmail.com>
2022-11-05 11:58:14 +0100

    Dodat stil na stranicu

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Automobili</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+  </head>
+  <body>
+    <div class="wrapper">
+      <h1 class="heading">Tabela automobila</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Proizvodjac</th>
+            <th>Model</th>
+            <th>Godina proizvodnje</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Audi</td>
+            <td>A4</td>
+            <td>2015</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th colspan="2">Ukupno rezultata:</th>
+            <th>1</th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  </body>
+</html>
